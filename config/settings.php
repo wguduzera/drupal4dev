244d0e6c79b8399d64ded9b6f4cdcef05c4ce021
Everton Rodrigues Garcia <everton.garcia@trt12.jus.br>
2024-05-15 13:35:32 -0300

    86a3dbc7u

--- config/settings.php
+++ config/settings.php
@@ -898,3 +898,5 @@ $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml'
 $settings['cache']['bins']['render'] = 'cache.backend.null';
 $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
 $settings['cache']['bins']['page'] = 'cache.backend.null';
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
